caa3fd54794367c62196dd319bbb742b95b5d814
clementmarin <clement.marin@laplateforme.io>
2023-09-17 11:44:04 +0200

    [UDPATED] Dungeon Generation Algo
    Working sample

--- Assets/Scripts/Room/DungeonManager.cs
+++ Assets/Scripts/Room/DungeonManager.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -6,7 +6,7 @@ public class DungeonManager : MonoBehaviour
 {
    //! Singleton
    public static DungeonManager Instance { get; private set; }
-     private void Awake()
+   private void Awake()
    {
       if (Instance == null)
       {
@@ -26,25 +26,23 @@ public class DungeonManager : MonoBehaviour
    [SerializeField] private int rooms;
    [SerializeField] private RoomData[] roomsData;
    [SerializeField] private RoomData startRoom;
-
+   private readonly Vector2[] _directions = {Vector2.up, Vector2.down, Vector2.left, Vector2.right};
 
 
    private RoomData[,] _roomsLayout;
-   private bool[,] _roomsVisited;
    private Vector2 _currentRoomPosition;
 
    // Live variables
-
+   private List<bool> _roomsCleared;
 
    private void Start()
    {
       _roomsLayout = new RoomData[rooms * 2, rooms * 2];
       GenerateDungeonLayout();
-      DebugLayout();
    }
 
-   // Generate the dungeon
-   private void GenerateDungeon()
+   // Generate the Room
+   private void GenerateRoom()
    {
       // instantiate as child of dungeon manager
    }
@@ -64,80 +62,64 @@ public class DungeonManager : MonoBehaviour
 
    private void GenerateDungeonLayout()
    {
+      //! Debug Time
       float startTime = Time.realtimeSinceStartup;
-      Vector2 currentRoomGenerationPosition = new Vector2(rooms / 2, rooms / 2);
-      bool asGeneratedaARoom = false;
-      // Create the start room
-      _roomsLayout[rooms / 2, rooms / 2] = startRoom;
-      Debug.Log("Start room generated");
 
+      Vector2 currentRoomGenerationPosition = new Vector2(rooms / 2, rooms / 2);
+      _roomsLayout[rooms / 2, rooms / 2] = startRoom;
       // Create the rest of the rooms
-      for (int i = 0; i < rooms; i++)
+      for (int i = 1; i < rooms; i++)
       {
-         // check if room are already created around the current room
-         if (_roomsLayout[(int)currentRoomGenerationPosition.x + 1, (int)currentRoomGenerationPosition.y] == null)
+         bool roomGenerated = TryGenerateRoomInAllDirections(currentRoomGenerationPosition);
+         if (Random.Range(0, 4) == 0 || !roomGenerated)
          {
-            // Create a room to the right
-            _roomsLayout[(int)currentRoomGenerationPosition.x + 1, (int)currentRoomGenerationPosition.y] =
-               GetRandomRoomData();
-            asGeneratedaARoom = true;
+            currentRoomGenerationPosition += _directions[Random.Range(0, _directions.Length)];
          }
-         else if (_roomsLayout[(int)currentRoomGenerationPosition.x - 1, (int)currentRoomGenerationPosition.y] == null)
-         {
-            // Create a room to the left
-            _roomsLayout[(int)currentRoomGenerationPosition.x - 1, (int)currentRoomGenerationPosition.y] =
-               GetRandomRoomData();
-            asGeneratedaARoom = true;
-         }
-         else if (_roomsLayout[(int)currentRoomGenerationPosition.x, (int)currentRoomGenerationPosition.y + 1] == null)
-         {
-            // Create a room to the top
-            _roomsLayout[(int)currentRoomGenerationPosition.x, (int)currentRoomGenerationPosition.y + 1] =
-               GetRandomRoomData();
-            asGeneratedaARoom = true;
-         }
-         else if (_roomsLayout[(int)currentRoomGenerationPosition.x, (int)currentRoomGenerationPosition.y - 1] == null)
-         {
-            // Create a room to the bottom
-            _roomsLayout[(int)currentRoomGenerationPosition.x, (int)currentRoomGenerationPosition.y - 1] =
-               GetRandomRoomData();
-            asGeneratedaARoom = true;
-         }
-         // have a one to four chance to change the direction of the room generation
-         if (Random.Range(0, 4) == 0)
-         {
-            // change the direction of the room generation by one unit
-            currentRoomGenerationPosition += new Vector2(Random.Range(-1, 1), Random.Range(-1, 1));
-            asGeneratedaARoom = false;
-         }
-         if (!asGeneratedaARoom)
+         if (!roomGenerated)
          {
             i--;
          }
-         Debug.Log("Room " + i + " generated");
       }
-      Debug.Log("Dungeon layout generated" + " in " + (Time.realtimeSinceStartup - startTime) + " seconds");
+      Debug.Log("<color=green>Generated dungeon layout in " + (Time.realtimeSinceStartup - startTime) + " seconds</color>");
+      DebugPrintDungeonLayout();
    }
-   void DebugLayout()
+   private bool TryGenerateRoomInAllDirections(Vector2 generationPosition)
    {
-      for (int i = 0; i < rooms * 2; i++)
+      foreach (Vector2 direction in _directions)
       {
-         for (int j = 0; j < rooms * 2; j++)
+         if (IsRoomInDirectionEmpty(generationPosition, direction))
+         {
+            GenerateRoomInDirection(generationPosition, direction);
+            return true;
+         }
+      }
+      return false;
+   }
+
+   private bool IsRoomInDirectionEmpty(Vector2 generationPosition, Vector2 direction)
+   {
+      return _roomsLayout[(int)generationPosition.x + (int)direction.x, (int)generationPosition.y + (int)direction.y] == null;
+   }
+
+   private void GenerateRoomInDirection(Vector2 generationPosition, Vector2 direction)
+   {
+      _roomsLayout[(int)generationPosition.x + (int)direction.x, (int)generationPosition.y + (int)direction.y] =
+         GetRandomRoomData();
+   }
+
+   private void DebugPrintDungeonLayout()
+   {
+      for (int i = 0; i < _roomsLayout.GetLength(0); i++)
+      {
+         for (int j = 0; j < _roomsLayout.GetLength(1); j++)
          {
             if (_roomsLayout[i, j] != null)
             {
-               Debug.Log("Room at " + i + " " + j + " is " + _roomsLayout[i, j].name);
+               Debug.Log("<color=yellow>Room at </color>" + i + ", " + j + " is " + "<color=red>" + _roomsLayout[i, j].name + "</color>");
             }
          }
       }
    }
-
-   private void CreateRoom(RoomData roomData)
-   {
-      // Create the room
-      GameObject room = Instantiate(roomData.roomPrefab, transform);
-   }
-
 
    public void ChangeRoom(Vector2 direction)
    {
