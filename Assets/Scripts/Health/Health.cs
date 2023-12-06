0fd4ad78096cecde6c93be2b0ac8d6657188ad61
clementmarin <clement.marin@laplateforme.io>
2023-12-06 14:42:24 +0100

    [REFACTOR] Health code

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -5,29 +5,32 @@ using static UnityEngine.Mathf;
 
 public class Health : MonoBehaviour
 {
-    
-    enum ObjectType{ Player, Enemy, Object }
+    private enum ObjectType
+    {
+        Player,
+        Enemy,
+        Object
+    }
 
-    private ObjectType healthType;
+    private ObjectType _healthType;
 
-    [Header("Health")]
+    [Header("Health")] [SerializeField] private float maxHealth = 100;
 
-    [SerializeField] private float maxHealth = 100;
+    private float _health;
 
-    private float health;
+    [Header("Enemy Reference")] [SerializeField]
+    private GameObject damagePopup;
 
-    [Header("Enemy Reference")]
-    [SerializeField] private GameObject damagePopup;
+    [Header("(DEBUG) Dummy")] [SerializeField]
+    public bool isDummy;
 
-    [Header("(DEBUG) Dummy")]
-    [SerializeField] public bool isDummy;
     [SerializeField] private float dummyCooldown = 3f;
-    private float dummyCooldownTimer;
+    private float _dummyCooldownTimer;
 
-    [Header("Status Effects")]
+    [Header("Status Effects")] [SerializeField]
+    private float statusEffectTickRate = 1f;
 
-    [SerializeField] private float statusEffectTickRate = 1f;
-    private float statusEffectTimer;
+    private float _statusEffectTimer;
 
     // * Poison
     [SerializeField] public bool isPoisoned;
@@ -35,11 +38,11 @@ public class Health : MonoBehaviour
     [SerializeField] private float poisonDuration;
     [SerializeField] private int maxPoisonStacks = 5;
     [SerializeField] private int poisonStacks;
-    
+
     private void Start()
     {
-        health = maxHealth;
-        this.healthType = gameObject.tag switch
+        _health = maxHealth;
+        _healthType = gameObject.tag switch
         {
             "Player" => ObjectType.Player,
             "Enemy" => ObjectType.Enemy,
@@ -52,33 +55,31 @@ public class Health : MonoBehaviour
     private void Update()
     {
         // ! This is a temporary solution
-        if (isDummy)
+        if (!isDummy) return;
+        if (_dummyCooldownTimer > 0)
         {
-            if (dummyCooldownTimer > 0)
-            {
-                dummyCooldownTimer -= Time.deltaTime;
-            }
-            else
-            {
-                dummyCooldownTimer = dummyCooldown;
-                health = maxHealth;
-            }
+            _dummyCooldownTimer -= Time.deltaTime;
+        }
+        else
+        {
+            _dummyCooldownTimer = dummyCooldown;
+            _health = maxHealth;
         }
     }
 
     public void TakeDamage(List<object> damageInfo)
     {
         // * 0 = Damage, 1 = IsCritical, 3 = ArrowEffects
-        float damage = (float) damageInfo[0];
-        bool arrowisCritical = (bool) damageInfo[1];
+        float damage = (float)damageInfo[0];
+        bool arrowisCritical = (bool)damageInfo[1];
 
-        // * (ArrowEffects) 0 = Poison, 1 = Burning, 2 = Freezing, 3 = Electrifying 
-        bool[] arrowEffects = (bool[]) damageInfo[2];                                                     
+        // * (ArrowEffects) 0 = Poison, 1 = Burning, 2 = Freezing, 3 = Electrifying
+        bool[] arrowEffects = (bool[])damageInfo[2];
         bool arrowIsPoisoned = arrowEffects[0];
         bool arrowIsBurning = arrowEffects[1];
         bool arrowIsFreezing = arrowEffects[2];
         bool arrowIsElectrifying = arrowEffects[3];
-        
+
         if (arrowIsPoisoned)
         {
             isPoisoned = true;
@@ -93,141 +94,168 @@ public class Health : MonoBehaviour
             }
         }
 
-        if (healthType == ObjectType.Player)
+        switch (_healthType)
         {
-            health -= RoundToInt(damage);
-            GameManager.Instance.player.GetComponent<Player_Behaviour>().playerCamera.ShakeCamera(0.1f, 0.2f);
-            GameObject damagePopupInstance = Instantiate(damagePopup, transform.position, Quaternion.identity);
-            DamageFloatingText floatingText = damagePopupInstance.GetComponent<DamageFloatingText>();
-            floatingText.Damage = damage;
-            floatingText.IsCritical = arrowisCritical;
-            floatingText.Color = Color.white;
-        }
-        else if (healthType == ObjectType.Enemy)
-        {
-            health -= RoundToInt(damage);
-            GameObject damagePopupInstance = Instantiate(damagePopup, transform.position, Quaternion.identity);
-            DamageFloatingText floatingText = damagePopupInstance.GetComponent<DamageFloatingText>();
-            floatingText.Damage = damage;
-            floatingText.IsCritical = arrowisCritical;
-            floatingText.Color = Color.white;
-
-            // ! This is a temporary solution
-            if (isDummy)
+            case ObjectType.Player:
             {
-                dummyCooldownTimer = dummyCooldown;
+                _health -= RoundToInt(damage);
+                GameManager.Instance.player.GetComponent<Player_Behaviour>().playerCamera.ShakeCamera(0.1f, 0.2f);
+                GameObject damagePopupInstance = Instantiate(damagePopup, transform.position, Quaternion.identity);
+                DamageFloatingText floatingText = damagePopupInstance.GetComponent<DamageFloatingText>();
+                floatingText.Damage = damage;
+                floatingText.IsCritical = arrowisCritical;
+                floatingText.Color = Color.white;
+                break;
             }
-            // !
-        }
-        else if (healthType == ObjectType.Object)
-        {
-            health -= damage;
-        }
-        else
-        {
-            Debug.Log("ObjectType not found");
+            case ObjectType.Enemy:
+            {
+                _health -= RoundToInt(damage);
+                GameObject damagePopupInstance = Instantiate(damagePopup, transform.position, Quaternion.identity);
+                DamageFloatingText floatingText = damagePopupInstance.GetComponent<DamageFloatingText>();
+                floatingText.Damage = damage;
+                floatingText.IsCritical = arrowisCritical;
+                floatingText.Color = Color.white;
+
+                // ! This is a temporary solution
+                if (isDummy)
+                {
+                    _dummyCooldownTimer = dummyCooldown;
+                }
+
+                // !
+                break;
+            }
+            case ObjectType.Object:
+                _health -= damage;
+                break;
+            default:
+                Debug.Log("ObjectType not found");
+                break;
         }
 
         // check if dead
-        if (health <= 0 && !isDummy) // Todo: Remove !isDummy
+        if (!(_health <= 0) || isDummy) return; // Todo: Remove !isDummy
+        DropItem dropItem = GetComponent<DropItem>();
+        if (dropItem != null)
         {
-            DropItem dropItem = GetComponent<DropItem>();
-            if (dropItem != null)
-            {
-                dropItem.GenerateItem();
-            }
-            Destroy(gameObject);
+            dropItem.GenerateItem();
         }
+
+        Destroy(gameObject);
     }
 
     private void TakeStatusEffectDamage(float damage)
     {
         if (isPoisoned)
         {
-            if (healthType == ObjectType.Player)
+            switch (_healthType)
             {
-                health -= damage * poisonStacks;
-            }
-            else if (healthType == ObjectType.Enemy)
-            {
-                health -= damage * poisonStacks;
-                GameObject damagePopupInstance = Instantiate(damagePopup, transform.position, Quaternion.identity);
-                DamageFloatingText floatingText = damagePopupInstance.GetComponent<DamageFloatingText>();
-                floatingText.Damage = damage * poisonStacks;
-                floatingText.Color = Color.green;
-
-                // ! This is a temporary solution
-                if (isDummy)
+                case ObjectType.Player:
+                    _health -= damage * poisonStacks;
+                    break;
+                case ObjectType.Enemy:
                 {
-                    dummyCooldownTimer = dummyCooldown;
+                    _health -= damage * poisonStacks;
+                    GameObject damagePopupInstance = Instantiate(damagePopup, transform.position, Quaternion.identity);
+                    DamageFloatingText floatingText = damagePopupInstance.GetComponent<DamageFloatingText>();
+                    floatingText.Damage = damage * poisonStacks;
+                    floatingText.Color = Color.green;
+
+                    // ! This is a temporary solution
+                    if (isDummy)
+                    {
+                        _dummyCooldownTimer = dummyCooldown;
+                    }
+
+                    // !
+                    break;
                 }
-                // !
-            }
-            else
-            {
-                Debug.Log("ObjectType not found");
+                case ObjectType.Object:
+                    _health -= damage * poisonStacks;
+                    break;
+                default:
+                    Debug.Log("ObjectType not found");
+                    break;
             }
         }
 
         // check if dead
-        if (health <= 0)
+        if (!(_health <= 0)) return;
+
+        DropItem dropItem = GetComponent<DropItem>();
+        if (dropItem != null)
         {
-            DropItem dropItem = GetComponent<DropItem>();
-            if (dropItem != null)
-            {
-                dropItem.GenerateItem();
-            }
-            Destroy(gameObject);
+            dropItem.GenerateItem();
         }
+
+        Destroy(gameObject);
     }
 
     public void Heal(float healAmount)
     {
-        health += healAmount;
+        _health += healAmount;
         GameObject damagePopupInstance = Instantiate(damagePopup, transform.position, Quaternion.identity);
         DamageFloatingText floatingText = damagePopupInstance.GetComponent<DamageFloatingText>();
         floatingText.Damage = healAmount;
         floatingText.Color = Color.magenta;
-        if (health > maxHealth)
+        if (_health > maxHealth)
         {
-            health = maxHealth;
+            _health = maxHealth;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (healthType == ObjectType.Player)
+        switch (_healthType)
         {
-            switch (col.gameObject.tag)
-            {
-                case "Enemy":
-                    TakeDamage(new List<object>(){10f, false, new[]{false, false, false, false}});
-                    GameManager.Instance.player.GetComponent<Player_Behaviour>().playerMovement.Knockback(col.gameObject.transform.position);
-                    GameManager.Instance.player.GetComponent<Player_Behaviour>().playerCamera.ShakeCamera(0.2f, 0.1f);
-                    break;
-                case "EnemyProjectile":
-                    TakeDamage(new List<object>(){col.gameObject.GetComponent<Projectile_Behaviour>().projectileData.damage,false, new[]{false, false, false, false}});
-                    break;
-            }
-        }
-        else if (healthType == ObjectType.Enemy)
-        {
-            switch (col.gameObject.tag)
-            {
-                case "PlayerProjectile":
-                    TakeDamage(col.gameObject.GetComponent<Projectile_Behaviour>().Damage);
-                    GameManager.Instance.player.GetComponent<Player_Behaviour>().playerCamera.ShakeCamera(0.1f, 0.05f);
-                    break;
-            }
-        }
-        else
-        {
-            Debug.Log("ObjectType not found");
+            case ObjectType.Player:
+                switch (col.gameObject.tag)
+                {
+                    case "Enemy":
+                        TakeDamage(new List<object>() { 10f, false, new[] { false, false, false, false } });
+                        GameManager.Instance.player.GetComponent<Player_Behaviour>().playerMovement
+                            .Knockback(col.gameObject.transform.position);
+                        GameManager.Instance.player.GetComponent<Player_Behaviour>().playerCamera
+                            .ShakeCamera(0.2f, 0.1f);
+                        break;
+                    case "EnemyProjectile":
+                        TakeDamage(new List<object>()
+                        {
+                            col.gameObject.GetComponent<Projectile_Behaviour>().projectileData.damage, false,
+                            new[] { false, false, false, false }
+                        });
+                        break;
+                }
+
+                break;
+            case ObjectType.Enemy:
+                switch (col.gameObject.tag)
+                {
+                    case "PlayerProjectile":
+                        TakeDamage(col.gameObject.GetComponent<Projectile_Behaviour>().Damage);
+                        GameManager.Instance.player.GetComponent<Player_Behaviour>().playerCamera
+                            .ShakeCamera(0.1f, 0.05f);
+                        break;
+                }
+
+                break;
+            case ObjectType.Object:
+                switch (col.gameObject.tag)
+                {
+                    case "PlayerProjectile":
+                        TakeDamage(col.gameObject.GetComponent<Projectile_Behaviour>().Damage);
+                        break;
+                }
+
+                break;
+            default:
+                Debug.Log("ObjectType not found");
+                break;
         }
     }
 
     // ReSharper disable Unity.PerformanceAnalysis
-    IEnumerator StatusEffectTick()
+    private IEnumerator StatusEffectTick()
     {
         while (true)
         {
@@ -244,6 +272,7 @@ public class Health : MonoBehaviour
                     TakeStatusEffectDamage(5f);
                 }
             }
+
             yield return new WaitForSeconds(statusEffectTickRate);
         }
     }
@@ -256,7 +285,7 @@ public class Health : MonoBehaviour
 
     public float HealthValue
     {
-        get => health;
-        set => health = value;
+        get => _health;
+        set => _health = value;
     }
 }
